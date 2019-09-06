3ed00e81aa37c62f04ba33571100c50a024fb072
xianminxiao <xianminxiao@tencent.com>
2019-09-06 18:07:50 +0800

    test XMModel

--- XMBaseUtil/ViewController.m
+++ XMBaseUtil/ViewController.m
@@ -13,6 +13,27 @@
 
 @end
 
+@interface TestModel : XMModel
+
+@property(nonatomic, assign) NSUInteger age;
+@property(nonatomic, assign) BOOL       bTag;
+
+@end
+
+@implementation TestModel
+
+@end
+
+@interface TestSubModel : TestModel
+
+@property(nonatomic, strong) NSString*   info;
+
+@end
+
+@implementation TestSubModel
+
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad {
@@ -21,6 +42,26 @@
     
     XMModel* model = [XMModel new];
     model = nil;
+
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //    TestModel* testModel = [TestModel new];
+    //    testModel.bTag = YES;
+    //    testModel.age = 20;
+    //
+    //    TestModel* testModel2 = [testModel mutableCopy];
+    //    testModel2 = nil;
+    
+
+    TestSubModel* testSubModel = [TestSubModel new];
+    testSubModel.info = @"xxmtest";
+    testSubModel.age = 19;
+    testSubModel.bTag = YES;
+    
+    TestSubModel* testSubModel2 = [testSubModel mutableCopy];
+    testSubModel2 = nil;
 }
 
 
